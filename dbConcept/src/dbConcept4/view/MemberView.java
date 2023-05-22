4b6413b5d88cc5e7d2543576d518a2530ae3bdfb
jeongwon <rladnjs15@naver.com>
2023-05-22 23:30:34 +0900

    Merge branch 'master' of https://github.com/k-gardn/java_basic.git

--- dbConcept/src/dbConcept4/view/MemberView.java
+++ dbConcept/src/dbConcept4/view/MemberView.java
@@ -12,12 +12,13 @@ public class MemberView implements IView {
 	public void mainMenu() {
 		
 		while (true) {
-			System.out.println("1. 회원 등록");
-			System.out.println("2. 회원 검색");
-			System.out.println("3. 회원 삭제");
+			
+			System.out.println("1. 회원 전체 정보 출력");
+			System.out.println("2. 회원 정보 조회");
+			System.out.println("3. 회원 정보 입력");
 			System.out.println("4. 회원 모두 보기");
-			System.out.println("5. 회원 수정");
-			System.out.println("6. 프로그램 종료");
+			System.out.println("5. 회원 정보 삭제");
+			System.out.println("0. 프로그램 종료");
 			System.out.print("선택 > ");
 			int select;
 			try {
@@ -26,12 +27,12 @@ public class MemberView implements IView {
 				continue;
 			}
 			switch (select) {
-			case 1:	insertMember(); break;
-			case 2:	selectId(); break;
-			case 3:	deleteMember(); break;
-			case 4:	selectAll(); break;
-			case 5: updateMember(); break;
-			case 6: disconnection();
+			case 1:	selectAll(); break; 	// 1. 회원 전체 정보 출력
+			case 2:	selectId(); break; 		// 2. 회원 정보 조회
+			case 3:	insertMember(); break;  // 3. 회원 정보 입력
+			case 4:	selectAll(); break;		// 4. 회원 모두 보기
+			case 5: deleteMember(); break;	// 5. 회원 정보 삭제
+			case 0: disconnection();
 					System.out.println("프로그램을 종료합니다.");
 					System.exit(0);
 			default:
